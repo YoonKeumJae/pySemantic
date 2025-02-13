b5f87a20e14379c383b2f860f0befa3c5dbe3aae
YoonKeumJae <ykj1512@gmail.com>
2025-02-13 11:43:56 +0900

    local Ollama(llama3) connect

---
+++ main.py
@@ -0,0 +1,66 @@
+import asyncio
+import logging
+from semantic_kernel import Kernel
+from semantic_kernel.utils.logging import setup_logging
+from semantic_kernel.functions import kernel_function
+from semantic_kernel.connectors.ai.ollama import OllamaChatCompletion
+from semantic_kernel.contents.chat_history import ChatHistory
+
+from semantic_kernel.connectors.ai.ollama import (
+    OllamaPromptExecutionSettings
+)
+
+async def main():
+    kernel = Kernel()
+    chat_completion = OllamaChatCompletion(
+        ai_model_id="llama3",
+        host="http://127.0.0.1:11434",
+    )
+    kernel.add_service(chat_completion)
+
+    # Set the logging level for  semantic_kernel.kernel to DEBUG.
+    setup_logging()
+    logging.getLogger("kernel").setLevel(logging.DEBUG)
+
+    # Add a plugin (the LightsPlugin class is defined below)
+    # kernel.add_plugin(
+    #     LightsPlugin(),
+    #     plugin_name="Lights",
+    # )
+
+    # Enable planning
+    execution_settings = OllamaPromptExecutionSettings(service_id="ollama",model_id="llama3")
+    # execution_settings.function_call_behavior = FunctionChoiceBehavior.Auto()
+
+    # Create a history of the conversation
+    history = ChatHistory()
+
+    # Initiate a back-and-forth chat
+    userInput = None
+    while True:
+        # Collect user input
+        userInput = input("User > ")
+
+        # Terminate the loop if the user says "exit"
+        if userInput == "exit":
+            break
+
+        # Add user input to the history
+        history.add_user_message(userInput)
+
+        # Get the response from the AI
+        result = await chat_completion.get_chat_message_content(
+            chat_history=history,
+            settings=execution_settings,
+            kernel=kernel,
+        )
+
+        # Print the results
+        print("Assistant > " + str(result))
+
+        # Add the message from the agent to the chat history
+        history.add_message(result)
+
+# Run the main function
+if __name__ == "__main__":
+    asyncio.run(main())
